6b74feebbd7962f49b50428dbff54bfe1ae9f28d
xiaohutongxue <929125066@qq.com>
2019-11-05 20:45:27 +0800

    LeetCode110平衡二叉树

---
+++ LeetCode110平衡二叉树.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
+    }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left == null && root.right == null) {
+            return true;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        if (leftDepth - rightDepth > 1 || rightDepth - leftDepth > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
