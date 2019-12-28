47d9c4c491a00d650f3417badcd9bfdd48e20a65
xiaohutongxue <929125066@qq.com>
2019-12-28 16:14:43 +0800

    Create 488.找到消失数字.java

---
+++ 488.找到消失数字.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[Math.abs(nums[i]) - 1] > 0){
+                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+    }
+}
