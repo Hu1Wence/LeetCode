834d934aece0af30242565f59692821db68f78b5
xiaohutongxue <929125066@qq.com>
2020-01-01 20:58:33 +0800

    Create 168. Excel表列名称.java

---
+++ 168. Excel表列名称.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public String convertToTitle(int n) {
+        if (n <= 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            n--;
+            sb.append((char) (n % 26 + 'A'));
+            n = n / 26;
+        }
+        return sb.reverse().toString();
+    }
+}
