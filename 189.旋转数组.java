84dc322ee22c0d114b77a6569147f489871a15a3
xiaohutongxue <929125066@qq.com>
2020-01-03 22:31:48 +0800

    Create 189.旋转数组.java

---
+++ 189.旋转数组.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public void rotate(int[] nums, int k) {
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k % nums.length - 1);
+        reverse(nums, k % nums.length, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+
+}
