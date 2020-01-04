08c693de2782af6df303b961f8566a14fa515c85
xiaohutongxue <929125066@qq.com>
2020-01-04 23:06:35 +0800

    Create 204. 计数质数.java

---
+++ 204. 计数质数.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = 2 * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
