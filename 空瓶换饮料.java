5cba35e73b8b3f93e300dc18c6f39fa39844a417
xiaohutongxue <929125066@qq.com>
2020-03-02 21:29:26 +0800

    Create 空瓶换饮料.java

---
+++ 空瓶换饮料.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int count(int[] A, int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (A[i] > A[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
